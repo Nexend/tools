31c4ecebac6e33e08d76de08adb8284b28be0a0d
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-01-16 19:35:17 +0300

    task1-fix

--- lesson5-webpack-loaders/task1-webpack-css-loader/webpack.config.js
+++ lesson5-webpack-loaders/task1-webpack-css-loader/webpack.config.js
@@ -15,9 +15,9 @@ module.exports = {
           {
             loader: "url-loader",
             options: {
-              limit: 8000,
+              limit: 8192,
               name: "[name].[ext]",
-              outputPathL: "images",
+              outputPath: "images",
             },
           },
         ],
